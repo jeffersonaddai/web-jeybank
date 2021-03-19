94adaba0c64c940c9a98b848d1d9b4241c718a4a
jefferson <jeffepok@gmail.com>
2021-03-19 16:36:32 +0000

    Added more promo slides

--- scripts/account.js
+++ scripts/account.js
@@ -72,19 +72,3 @@ function deposit(){
   amountInp.value = "";
   alert("Deposit of Ghs " + depositAmount + " made.\nNew balance is Ghs " + localStorage.getItem("balance"));
 }
-
-function getCookie(cname) {
-    var name = cname + "=";
-    var ca = document.cookie.split(';');
-    for(var i = 0; i < ca.length; i++) {
-      var c = ca[i];
-      while (c.charAt(0) == ' ') {
-        c = c.substring(1);
-      }
-      if (c.indexOf(name) == 0) {
-        return c.substring(name.length, c.length);
-      }
-    }
-    return "";
-  }
-
--- scripts/slideShow.js
+++ scripts/slideShow.js
@@ -1,16 +1,30 @@
-var index = 0;
+var slidesIndex = 0;
+var coverSlidesIndex = 0;
 slideShow();
-
+coverSlideShow();
 function slideShow() {
     var i;
     var slides = document.getElementsByClassName("slides");
     for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";  
     }
-    index++;
-    if (index > slides.length) {
-        index = 1
+    slidesIndex++;
+    if (slidesIndex > slides.length) {
+        slidesIndex = 1
     }
-    slides[index-1].style.display = "block";  
+    slides[slidesIndex-1].style.display = "block";  
     setTimeout(slideShow, 8000); // Change image every 2 seconds
+}
+function coverSlideShow() {
+    var i;
+    var slides = document.getElementsByClassName("cover-slides");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";  
+    }
+    coverSlidesIndex++;
+    if (coverSlidesIndex > slides.length) {
+        coverSlidesIndex = 1
+    }
+    slides[coverSlidesIndex-1].style.display = "block";  
+    setTimeout(coverSlideShow, 5000); // Change image every 2 seconds
 }
